84cfd71ab51abf69e413838104e65624fbd5e493
theGANOUSH <babaganoush45@outlook.com>
2018-04-14 11:08:22 -0400

    Created databaseQuery method

--- Project4-2/src/common/RequestHandler.java
+++ Project4-2/src/common/RequestHandler.java
@@ -25,35 +25,38 @@ public class RequestHandler extends HttpServlet{
 	
 	private static final long serialVersionUID = 3004342931828496260L;
 	private Connection connection;
-	private ResultSet resultSet;
+	
+	public ResultSet databaseQuery(String query, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ResultSet resultSet = null;
+		
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setUser("client");
+		dataSource.setPassword("Password1");
+		dataSource.setURL("jdbc:mysql://localhost:3306/project4");
+		try {
+			connection = dataSource.getConnection();
+
+			}catch (Exception e){
+				request.setAttribute("output",e.getMessage());
+				request.getRequestDispatcher("/index.jsp").forward(request, response);System.err.println(e.getMessage());
+			}
+		try {
+			Statement statement = connection.createStatement();
+			resultSet = statement.executeQuery(query);
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+				request.setAttribute("output",e.getMessage());
+				request.getRequestDispatcher("/index.jsp").forward(request, response);
+			}
+		
+		return resultSet;
+	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		 response.setContentType("text/html");
 		 String input = request.getParameter("input");
-		 String output;
 		 
-		 MysqlDataSource dataSource = new MysqlDataSource();
-		 dataSource.setUser("client");
-		 dataSource.setPassword("Password1");
-		 dataSource.setURL("jdbc:mysql://localhost:3306/project4");
-		 
-		 try {
-			 connection = dataSource.getConnection();
-			 output = "Database connected!";
-		 }catch (Exception e)
-		 {
-			 output = e.getMessage();
-		 }
-		 
-		 try {
-			Statement statement = connection.createStatement();
-			statement.executeQuery(input);			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			output = (e.getMessage());
-		}
-		 
-		 request.setAttribute("output", output);
+		 request.setAttribute("output",databaseQuery(input, request, response));
 		 request.getRequestDispatcher("/index.jsp").forward(request, response);
 	 }
 
