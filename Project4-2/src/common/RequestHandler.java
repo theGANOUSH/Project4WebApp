bfced235e19e6acc85598ae69f4ffa6556d4488f
theGANOUSH <babaganoush45@outlook.com>
2018-04-13 23:16:46 -0400

    Added database connection settings.

--- Project4-2/src/common/RequestHandler.java
+++ Project4-2/src/common/RequestHandler.java
@@ -4,10 +4,13 @@
 package common;
 
 import java.io.IOException;
+import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 /**
  * @author Austin
@@ -16,12 +19,26 @@ import javax.servlet.http.HttpServletResponse;
 public class RequestHandler extends HttpServlet{
 	
 	private static final long serialVersionUID = 3004342931828496260L;
+	private Connection connection;
+	//private ResultSet resultSet;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		 response.setContentType("text/html");
-		 
 		 String input = request.getParameter("input");
 		 
+		 MysqlDataSource dataSource = new MysqlDataSource();
+		 dataSource.setUser("");
+		 dataSource.setPassword("");
+		 //CorrectIP
+		 dataSource.setURL("jdbc:mysql://:3306/project4");
+		 
+		 try {
+			 connection = dataSource.getConnection();
+			 input = "Database connected!";
+		 }catch (Exception e)
+		 {
+			 input = "Did not Connect";
+		 }
 		 request.setAttribute("output", input);
 		 request.getRequestDispatcher("/index.jsp").forward(request, response);
 	 }
